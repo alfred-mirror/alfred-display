26798e1bd374d270a39ba5503f39a5dad6079a7d
Natalie Chow <cychownat@gmail.com>
2016-03-07 12:37:51 -0800

    More client sockets

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
+const io = require('socket.io-client');
 var socket = io.connect('http://localhost:8080');
-console.log(typeof socket);
+
+socket.on('configChange', function(configData) {});
+
 
 
 // building widgets
-var time = require(__dirname + '/widgets/time_and_date');
+const time = require(__dirname + '/widgets/time_and_date');
 
 time('module-0');
 time('module-1');
